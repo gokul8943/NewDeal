b14950c135d7cb92f27576f287906bd0f546f208
Gokul M R <gokulmr331@gmail.com>
2024-09-24 12:31:46 +0530

    db connection added

--- src/framework/index.ts
+++ src/framework/index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { dbConnection } from './database/dbconnection';
 
 dotenv.config();
 
 const app: Express = express();
 const port: string = process.env.PORT!;
+
+dbConnection();
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
