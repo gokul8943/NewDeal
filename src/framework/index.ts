b2a5ce56c1d3c848dae8d560414e498685b1e71a
Gokul M R <gokulmr331@gmail.com>
2024-11-05 16:45:23 +0530

    Added the methods in corsoptions

--- src/framework/index.ts
+++ src/framework/index.ts
@@ -16,6 +16,7 @@ dbConnection();
 const corsOptions = {
   origin: ['http://localhost:5173'],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 };
 
 app.use(express.json({ limit: '20mb' }));
